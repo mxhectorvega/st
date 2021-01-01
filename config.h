72f5135e57ce5f25f05a58226f5b200005530431
mxhectorvega <mxhectorvega@gmail.com>
2021-01-01 17:05:30 -0600

    actualizacion

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "sf mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "SF Mono:pixelsize=14:antialias=true:autohint=true";
 static char *font2[] = { "Font Awesome:pixelsize=16:antialias=true:autohint=true" };
 static int borderpx = 2;
 
